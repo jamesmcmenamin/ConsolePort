80fee1a3e8f6dc5929779221218cfa0779108440
James McMenamin <reviir@gmail.com>
2019-07-22 20:28:02 -0500

    Add project files.

---
+++ ConsolePort/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ConsolePort.Models;
+
+namespace ConsolePort.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
